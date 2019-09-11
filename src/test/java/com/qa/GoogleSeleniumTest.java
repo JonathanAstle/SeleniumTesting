0fe0ba88f353e174812eadc1a96c81bb83f75cb4
Jonathan <jonathan.astle@academytrainee.com>
2019-09-11 11:38:14 +0100

    added test for exercises 1 and 2

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -55,4 +55,42 @@ public class GoogleSeleniumTest {
         js.executeScript("scroll(0, 4200)");       // scrolls TO position 3200, NOT by an additional 3200
         Thread.sleep(1000);
     }
+
+    @Test
+    public void myTestExercises1and2() throws InterruptedException {
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        Thread.sleep(800);
+
+        //first exercise on this page
+        WebElement textInput = driver.findElement(By.id("user-message"));
+        assertTrue(textInput.isDisplayed());
+        textInput.sendKeys("hello world!");
+        Thread.sleep(500);
+
+        WebElement showMessageButton = driver.findElement(By.xpath("//*[@id=\"get-input\"]/button"));
+        showMessageButton.click();
+        Thread.sleep(500);
+
+        WebElement displayedText = driver.findElement(By.id("display"));
+        assertTrue(displayedText.getText().equals("hello world!"));
+        Thread.sleep(1000);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1500)");
+
+        //second exercise on this page
+        WebElement firstInput = driver.findElement(By.id("sum1"));
+        WebElement secondInput = driver.findElement(By.id("sum2"));
+        firstInput.sendKeys("3");
+        secondInput.sendKeys("5");
+        Thread.sleep(400);
+
+        WebElement getTotalButton = driver.findElement(By.xpath("//*[@id=\"gettotal\"]/button"));
+        getTotalButton.click();
+        Thread.sleep(1000);
+
+        WebElement displayedTotal = driver.findElement(By.id("displayvalue"));
+        assertTrue(displayedTotal.getText().equals("8"));
+        Thread.sleep(1000);
+    }
 }
